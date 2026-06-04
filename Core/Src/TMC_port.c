fe15a074e4604d9a2c1a3a1dbed27c2f41c8302f
jakub s <stykajakub@gmail.com>
2026-06-04 21:14:52 +0200

    added some things need further test

--- Core/Src/TMC_port.c
+++ Core/Src/TMC_port.c
@@ -7,15 +7,38 @@ TMC5160BusType tmc5160_getBusType(uint16_t icID) {
     return IC_BUS_SPI;
 }
 
-void tmc5160_readWriteSPI(uint16_t icID, uint8_t *data, size_t dataLength) {
+// void tmc5160_readWriteSPI(uint16_t icID, uint8_t *data, size_t dataLength) {
 
-    HAL_GPIO_WritePin(TMC_CS_GPIO_Port, TMC_CS_Pin, GPIO_PIN_RESET);
+//     HAL_GPIO_WritePin(TMC_CS_GPIO_Port, TMC_CS_Pin, GPIO_PIN_RESET);
 
-    HAL_SPI_TransmitReceive(&hspi1, data, data, dataLength, 100);
+//     HAL_SPI_TransmitReceive(&hspi1, data, data, dataLength, 100);
 
-    HAL_GPIO_WritePin(TMC_CS_GPIO_Port, TMC_CS_Pin, GPIO_PIN_SET);
+//     HAL_GPIO_WritePin(TMC_CS_GPIO_Port, TMC_CS_Pin, GPIO_PIN_SET);
+// }
+void tmc5160_readWriteSPI(uint16_t icID, uint8_t *data, size_t writeLength, size_t readLength) {
+    size_t totalLength = (writeLength > readLength) ? writeLength : readLength;
+    
+      HAL_GPIO_WritePin(TMC_CS_GPIO_Port, TMC_CS_Pin, GPIO_PIN_RESET);
+      HAL_TransmitReceive_DMA(&hspi,data,data,totalLength);
+
+      ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+      HAL_GPIO_WritePin(TMC_CS_GPIO_Port, TMC_CS_Pin, GPIO_PIN_SET);
+
+    
 }
 
+void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi) {
+    
+    if(hspi->Instance == SPI1) {
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+        
+        // Wyślij sygnał pobudki do taska z silnikiem
+        vTaskNotifyGiveFromISR(defaultTaskHandle, &xHigherPriorityTaskWoken); 
+       
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    }
+}
 
 bool tmc5160_readWriteUART(uint16_t icID, uint8_t *data, size_t writeLength, size_t readLength) {
     return false;
