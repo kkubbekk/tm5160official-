b61ae43500e2ecee566bf6a81bb94e78ce9d5ef7
jakub s <stykajakub@gmail.com>
2026-06-04 20:14:33 +0200

    initial commit

---
+++ Core/Src/TMC_port.c
@@ -0,0 +1,27 @@
+#include "main.h"      // Ta linijka naprawia błędy z "SPI_HandleTypeDef" i brakiem HALa
+#include "TMC5160.h"
+
+extern SPI_HandleTypeDef hspi1;
+
+TMC5160BusType tmc5160_getBusType(uint16_t icID) {
+    return IC_BUS_SPI;
+}
+
+void tmc5160_readWriteSPI(uint16_t icID, uint8_t *data, size_t dataLength) {
+
+    HAL_GPIO_WritePin(TMC_CS_GPIO_Port, TMC_CS_Pin, GPIO_PIN_RESET);
+
+    HAL_SPI_TransmitReceive(&hspi1, data, data, dataLength, 100);
+
+    HAL_GPIO_WritePin(TMC_CS_GPIO_Port, TMC_CS_Pin, GPIO_PIN_SET);
+}
+
+
+bool tmc5160_readWriteUART(uint16_t icID, uint8_t *data, size_t writeLength, size_t readLength) {
+    return false;
+}
+
+uint8_t tmc5160_getNodeAddress(uint16_t icID) {
+    return 0;
+}
+
---
+++ Debug/Core/Src/TMC5160.d
@@ -0,0 +1,4 @@
+Core/Src/TMC5160.o: ../Core/Src/TMC5160.c ../Core/Inc/TMC5160.h \
+ ../Core/Inc/TMC5160_HW_Abstraction.h
+../Core/Inc/TMC5160.h:
+../Core/Inc/TMC5160_HW_Abstraction.h:
